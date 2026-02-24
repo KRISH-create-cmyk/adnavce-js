f20b9f0d1c559884910ad596ff17221c63519006
Krish <krishphogat14@gmail.com>
2026-02-24 10:02:30 +0530

    Comment out code in script.js
    Comment out various functions and event listeners in script.js.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 // const para=document.querySelector("p")
 // para.innerText="Hello Shubham"
 // para.style.backgroundColor="Black"
@@ -90,59 +91,220 @@
 //     clearInterval(timerID);
 // }, 10000);
 
-const name = document.querySelector("#name")
-const btn = document.querySelector("#btn")
-const list = document.querySelector(".list")
+// const name = document.querySelector("#name")
+// const btn = document.querySelector("#btn")
+// const list = document.querySelector(".list")
 
-btn.addEventListener("click", () => {
-    if (name.value == "") {
-        alert("Please enter a valid name")
-        return;
+// btn.addEventListener("click", () => {
+//     if (name.value == "") {
+//         alert("Please enter a valid name")
+//         return;
+//     }
+//     const li = document.createElement("li")
+//     const dlt = document.createElement("button")
+//     li.innerText = name.value;
+//     dlt.innerText = "Delete"
+//     dlt.addEventListener("click", () => {
+//         list.removeChild(li)
+//     })
+//     li.appendChild(dlt)
+//     list.appendChild(li)
+//     name.value = ""
+//     })
+
+// function print(num){
+//     setTimeout(() => {
+//         console.log("Inside print")
+//         num()
+// } ,2000)}
+
+// function sample(){
+//     console.log("Inside callback")
+// }
+
+// print(sample)
+// console.log("Starting Homework")
+
+// setTimeout(() => {
+//     console.log("Homework done");
+//     console.log("Starting dinner...");
+
+//     setTimeout(() => {
+//         console.log("Dinner done");
+//         console.log("Getting ready to go out...");
+
+//         setTimeout(() => {
+//             console.log("Going to playground");
+//         }, 1000);
+//     }, 2500);
+// }, 2000);
+
+
+// function finishHomework(callback){
+//     console.log("Starting homework...");
+//     setTimeout(() => {
+//         console.log("Homework finished");
+//         callback()
+//     }, 2000);
+// }
+
+// const p = new Promise((resolve, reject) => {
+//     let done = true;
+//     setTimeout(() => {
+//         if (done) {
+//             resolve("Homework is done");
+//         } else {
+//             reject("Homework is not done");
+//         }
+// }, 2000);});
+// p.then((data) => {
+//     console.log(data);
+// }).catch((er) =>{
+//     console.log(er);
+// }).finally(() => {
+//     console.log("Finally back")
+// })
+
+// function donehomwwork(){
+//     const p = new Promise((res , rej) => {
+//         let done = true;
+//         setTimeout(() => {
+//             if (done) {
+//                 console.log("Homework completed");
+//                 res("Homework is done");
+//             }else {
+//                 rej("Home not completed");
+//             }
+//         }, 2000)
+//     })
+//     return p
+// }
+
+// function eatDinner(){
+//     const p = new Promise((res, rej) => {
+//         let done = true;
+//         setTimeout(() => {
+//             if (done) {
+//                 console.log("Dinner completed");
+//                 res("Dinner is done");
+//             } else {
+//                 rej("Dinner not completed");
+//             }
+//         }, 2000)
+//     })
+//     return p
+// }
+
+// function goToPlayground(){
+//     const p = new Promise((res, rej) => {
+//         let done = true;
+//         setTimeout(() => {
+//             if (done) {
+//                 console.log("Going to playground");
+//                 res("Going to playground");
+//             } else {
+//                 rej("Not going to playground");
+//             }
+//         }, 2000)
+//     })
+//     return p
+// }
+
+// donehomwwork().then((data) => {
+//     console.log(data);
+//     return eatDinner();
+// }).then((data) => {
+//     console.log(data);
+//     return goToPlayground();
+// }).then((data) => {
+//     console.log(data);
+// }).catch((er) => {
+//     console.log(er);
+// }).finally(() => {
+//     console.log("Go to sleep")
+// })
+
+// function orderFood(){
+//     return new Promise((res, rej) => {
+//         setTimeout(() => {
+//             console.log("Food Ordered");
+//             res("food is ordered")
+//         }, 2000)
+//     })
+// }
+
+// function prepareFood(){
+//     return new Promise((res, rej) => {
+//         setTimeout(() => {
+//             console.log("Food Prepared");
+//             res()
+//         }, 3000)
+//     })
+// }
+
+// function deliverFood(){
+//     return new Promise((res, rej) => {
+//         setTimeout(() => {
+//             console.log("Food Delivered");
+//             res()
+//         }, 4000)
+//     })
+// }
+
+// async function foodOrder(){
+//     const data = await orderFood()
+//     console.log(data)
+//     await prepareFood()
+//     await deliverFood()
+// }
+
+// foodOrder()
+// console.log("First line")
+// try{
+//     let a = 10
+//     console.log(a)
+// } catch (error) {
+//     console.log("Error caught: " + error)
+// }
+// console.log("Last line")
+
+// try{
+//     let age = 16
+//     if (age < 18) {
+//         throw new Error("Age must be at least 18")
+//     }   
+// }catch(e){
+//     console.error("Error caught: " + e)
+// } 
+function* generator(){
+    yield 1
+    yield 2
+    yield 3
+}   
+const gen = generator()
+console.log(gen.next())
+console.log(gen.next())
+console.log(gen.next())
+console.log(gen.next())
+
+function* generator2(){
+    for(let i=1; i<=5; i++){
+        yield i
     }
-    const li = document.createElement("li")
-    const dlt = document.createElement("button")
-    li.innerText = name.value;
-    dlt.innerText = "Delete"
-    dlt.addEventListener("click", () => {
-        list.removeChild(li)
-    })
-    li.appendChild(dlt)
-    list.appendChild(li)
-    name.value = ""
-    })
-
-function print(num){
-    setTimeout(() => {
-        console.log("Inside print")
-        num()
-} ,2000)}
-
-function sample(){
-    console.log("Inside callback")
 }
+const gen2 = generator2()
+console.log(gen2.next())
+console.log(gen2.next())
+console.log(gen2.next())
+console.log(gen2.next())
+console.log(gen2.next())
+console.log(gen2.next()) 
 
-print(sample)
-console.log("Starting Homework")
-
-setTimeout(() => {
-    console.log("Homework done");
-    console.log("Starting dinner...");
-
-    setTimeout(() => {
-        console.log("Dinner done");
-        console.log("Getting ready to go out...");
-
-        setTimeout(() => {
-            console.log("Going to playground");
-        }, 1000);
-    }, 2500);
-}, 2000);
-
-
-function finishHomework(callback){
-    console.log("Starting homework...");
-    setTimeout(() => {
-        console.log("Homework finished");
-        callback()
-    }, 2000);
+function add(a){
+    return function(b){
+        return function(c){
+            return a+b+c
+        }   
+    }
 }
+console.log(add(2)(3)(4))
